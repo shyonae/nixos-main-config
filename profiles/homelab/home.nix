d1e8cb1cb4d01850f02a0fd0632924927b556689
shyonae <scionae@gmail.com>
2024-06-28 10:39:17 +0200

    restored files, modified homelab

--- profiles/homelab/home.nix
+++ profiles/homelab/home.nix
@@ -1,9 +1,23 @@
 { config, lib, pkgs, pkgs-stable, userSettings, ... }:
 {
-  imports = [
-    ../work/home.nix
-  ];
+  home.username = userSettings.username;
+  home.homeDirectory = "/home/" + userSettings.username;
 
-  stylixPkg.enable = lib.mkDefault false;
-  virtConfig.enable = lib.mkDefault false;
+  programs.home-manager.enable = true;
+
+  customScripts.enable = true;
+  git.enable = true;
+  htop.enable = true;
+  tmux.enable = true;
+  vim.enable = true;
+  zsh.enable = true;
+
+  home.stateVersion = "23.05";
+
+  home.sessionVariables = {
+    EDITOR = lib.mkForce userSettings.editor;
+    TERM = userSettings.term;
+    BROWSER = userSettings.browser;
+    MOZ_ENABLE_WAYLAND = 0;
+  };
 }	
