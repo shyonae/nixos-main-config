3b93622431314a2e78f6dabfeea3fc75de63da11
shyonae <scionae@gmail.com>
2024-07-11 23:22:35 +0200

    firewall exception for local network

--- modules/system/firewall.nix
+++ modules/system/firewall.nix
@@ -9,6 +9,14 @@
     # networking.firewall.allowedTCPPorts = [ ... ];
     # networking.firewall.allowedUDPPorts = [ ... ];
     # Or disable the firewall altogether.
-    networking.firewall.enable = true;
+    networking.firewall = {
+      enable = true;
+      extraCommands = ''
+        iptables -A nixos-fw -p tcp --source 192.168.1.0/24 -j nixos-fw-accept
+        iptables -A nixos-fw -p udp --source 192.168.1.0/24 -j nixos-fw-accept
+        iptables -A nixos-fw -p tcp --source 10.8.0.0/24 -j nixos-fw-accept
+        iptables -A nixos-fw -p udp --source 10.8.0.0/24 -j nixos-fw-accept
+      '';
+    };
   };
 }
