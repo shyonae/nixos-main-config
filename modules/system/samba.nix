81308fdf12f19e89905470b8c3f91686a236ea9d
shyonae <scionae@gmail.com>
2024-09-15 21:38:25 +0200

    fix: removed unused samba config

--- modules/system/samba.nix
+++ modules/system/samba.nix
@@ -14,11 +14,6 @@
         # See https://github.com/NixOS/nixpkgs/blob/592047fc9e4f7b74a4dc85d1b9f5243dfe4899e3/pkgs/top-level/all-packages.nix#L27268
         enable = true;
         openFirewall = true;
-        extraConfig = ''
-          server smb encrypt = required
-          # ^^ Note: Breaks `smbclient -L <ip/host> -U%` by default, might require the client to set `client min protocol`?
-          server min protocol = SMB3_00
-        '';
       };
     };
   };
